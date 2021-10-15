de5b1834fcc3c33e3df4e2a7bdec8e3fa24a619d
MURAVIAN\Anwar <office@muravian.com>
2021-10-16 01:59:02 +0300

    update

--- README.md
+++ README.md
@@ -2,3 +2,32 @@
 Code generates following files in CodeIgniter 4 : models, controllers, routes 
 
 Created to be used with React DataTable CRUD : https://www.npmjs.com/package/react-datatable-crud
+
+#Installing
+
+Because the package it's still under development in order to install it you'd need first to:
+```shell
+composer config minimum-stability dev
+```
+This will allow you to install dev packages. 
+
+```shell
+composer require muravian/cigen
+```
+
+Once installed you need to make sure that you connected to db and created your tables. 
+
+CiGen will generate models that will extend a base model in your app folder.
+
+To run the generator you'll need the following code somewhere in your controllers: 
+```injectablephp
+use \Muravia\CiGen\CiGen; // at top
+// And this somewhere in your controller
+
+$cigen = new CiGen();
+$cigen->index();
+```
+
+Run your controller and that's it. Your files are generated.
+
+After this you can successfully install https://www.npmjs.com/package/react-datatable-crud and enjoy your REACT DATATABLE CRUD component with backend API in Codeigniter. 
--- src/Config/Routes.php
+++ src/Config/Routes.php
@@ -2,27 +2,31 @@
 
 $routes->group('api', function ($routes) {
 
-    $routes->add('', '\Muravian\CiGen\Controllers\Api::defaultAnswer');
+    $routes->group('api', function ($routes) {
 
-    // Index
-    $routes->get("(:segment)", "\Muravian\CiGen\Controllers\Api::index/$1/$2");
+        $routes->add('', '\Muravian\CiGen\Controllers\Api::defaultAnswer');
 
-    // Show single
-    $routes->get('(:segment)/(:num)', '\Muravian\CiGen\Controllers\Api::show/$1/$2');
+        // Index
+        $routes->get("(:segment)", "\Muravian\CiGen\Controllers\Api::index/$1/$2");
 
-    // Create
-    $routes->put('(:segment)/create', '\Muravian\CiGen\Controllers\Api::create/$1');
+        // Show single
+        $routes->get('(:segment)/(:num)', '\Muravian\CiGen\Controllers\Api::show/$1/$2');
 
-    // Update Single
-    $routes->patch('(:segment)/update/(:num)', '\Muravian\CiGen\Controllers\Api::update/$1/$2');
+        // Create
+        $routes->put('(:segment)', '\Muravian\CiGen\Controllers\Api::create/$1');
 
-    // Delete single
-    $routes->delete('(:segment)/delete/(:num)', '\Muravian\CiGen\Controllers\Api::delete/$1/$2');
+        // Update Single
+        $routes->patch('(:segment)/(:num)', '\Muravian\CiGen\Controllers\Api::update/$1/$2');
 
-    // Delete Bulk
-    $routes->post('(:segment)/delete/', '\Muravian\CiGen\Controllers\Api::deleteBulk/$1');
+        // Delete single
+        $routes->delete('(:segment)/(:num)', '\Muravian\CiGen\Controllers\Api::delete/$1/$2');
 
-    // Fields
-    $routes->get("(:segment)/fields", "\Muravian\CiGen\Controllers\Api::fields/$1");
+        // Delete Bulk
+        $routes->post('(:segment)', '\Muravian\CiGen\Controllers\Api::deleteBulk/$1');
+
+        // Fields
+        $routes->get("(:segment)/fields", "\Muravian\CiGen\Controllers\Api::fields/$1");
+
+    });
 
 });
