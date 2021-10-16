cc839135d4ded3b41d900d04c4134467f747c8cf
MURAVIAN\Anwar <office@muravian.com>
2021-10-16 19:11:15 +0300

    update

--- src/Config/Routes.php
+++ src/Config/Routes.php
@@ -1,32 +1,32 @@
 <?php
 
+use Config;
+
+$routes = Services::routes();
+
 $routes->group('api', function ($routes) {
 
-    $routes->group('api', function ($routes) {
+    $routes->add('', '\Muravian\CiGen\Controllers\Api::defaultAnswer');
 
-        $routes->add('', '\Muravian\CiGen\Controllers\Api::defaultAnswer');
+    // Index
+    $routes->get("(:segment)", "\Muravian\CiGen\Controllers\Api::index/$1/$2");
 
-        // Index
-        $routes->get("(:segment)", "\Muravian\CiGen\Controllers\Api::index/$1/$2");
+    // Show single
+    $routes->get('(:segment)/(:num)', '\Muravian\CiGen\Controllers\Api::show/$1/$2');
 
-        // Show single
-        $routes->get('(:segment)/(:num)', '\Muravian\CiGen\Controllers\Api::show/$1/$2');
+    // Create
+    $routes->put('(:segment)', '\Muravian\CiGen\Controllers\Api::create/$1');
 
-        // Create
-        $routes->put('(:segment)', '\Muravian\CiGen\Controllers\Api::create/$1');
+    // Update Single
+    $routes->patch('(:segment)/(:num)', '\Muravian\CiGen\Controllers\Api::update/$1/$2');
 
-        // Update Single
-        $routes->patch('(:segment)/(:num)', '\Muravian\CiGen\Controllers\Api::update/$1/$2');
+    // Delete single
+    $routes->delete('(:segment)/(:num)', '\Muravian\CiGen\Controllers\Api::delete/$1/$2');
 
-        // Delete single
-        $routes->delete('(:segment)/(:num)', '\Muravian\CiGen\Controllers\Api::delete/$1/$2');
+    // Delete Bulk
+    $routes->post('(:segment)', '\Muravian\CiGen\Controllers\Api::deleteBulk/$1');
 
-        // Delete Bulk
-        $routes->post('(:segment)', '\Muravian\CiGen\Controllers\Api::deleteBulk/$1');
-
-        // Fields
-        $routes->get("(:segment)/fields", "\Muravian\CiGen\Controllers\Api::fields/$1");
-
-    });
+    // Fields
+    $routes->get("(:segment)/fields", "\Muravian\CiGen\Controllers\Api::fields/$1");
 
 });
